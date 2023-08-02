48cde7f0722fd0d4f097363acde749dec725a99a
zhangxiancheng <365839159@qq.com>
2023-08-03 07:45:19 +0800

    init

---
+++ EntityFrameworkCore/src/Aquarius.Application/AquariusApplicationModule.cs
@@ -0,0 +1,20 @@
+﻿using Volo.Abp.AutoMapper;
+using Volo.Abp.Modularity;
+
+namespace Aquarius;
+
+[DependsOn(
+    typeof(AquariusDomainModule),
+    typeof(AbpAutoMapperModule),
+    typeof(AquariusApplicationContractsModule)
+    )]
+public class AquariusApplicationModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<AquariusApplicationModule>();
+        });
+    }
+}
---
+++ EntityFrameworkCore/src/Aquarius.Domain.Shared/AquariusDomainSharedModule.cs
@@ -0,0 +1,36 @@
+﻿using Aquarius.Localization;
+using Volo.Abp.Localization;
+using Volo.Abp.Localization.ExceptionHandling;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Aquarius;
+
+[DependsOn(
+    typeof(AbpLocalizationModule)
+    )]
+public class AquariusDomainSharedModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AquariusDomainSharedModule>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Add<AquariusResource>("en")
+                .AddBaseTypes(typeof(AquariusResource))
+                .AddVirtualJson("/Localization/Aquarius");
+
+            options.DefaultResourceType = typeof(AquariusResource);
+        });
+
+        Configure<AbpExceptionLocalizationOptions>(options =>
+        {
+            options.MapCodeNamespace("Aquarius", typeof(AquariusResource));
+        });
+    }
+}
